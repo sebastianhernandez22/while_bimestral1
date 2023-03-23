c938715d29bc7b7544bcb8f2111445e0c2873a83
sebastianhernandez22 <sebastianhernandez@colegioguanenta.edu.co>
2023-03-23 18:30:49 -0500

    se agrego el diagrama, programa y README

---
+++ caso_1/README.md
@@ -0,0 +1,5 @@
+# caso_1
+programa para hallar la suma de los primeros N numeros naturales
+
+# Diagrama de flujo
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ caso_1/caso_1.py
@@ -0,0 +1,15 @@
+# input
+
+N = int(input("Dijite el valor de N: "))
+suma = 0
+i = 1
+
+# proccesing
+
+while(i<=N):
+    suma = suma + i
+    i = i + 1
+
+# output
+
+print("la suma es: " + str(suma))
